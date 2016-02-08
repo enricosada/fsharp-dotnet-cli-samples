cdd7ae1129f14aaaf3d7dec54f9f9efef0ce25ce
Enrico Sada <enrico@sada.io>
2016-02-08 17:56:47 +0100

    add travis

---
+++ scripts/ci-build.sh
@@ -0,0 +1,16 @@
+
+#make path absolute
+
+# check current dotnet
+
+echo 'current dotnet:'
+type dotnet
+
+# download and build cli
+
+# use dotnet cli
+
+# check dotnet exe
+
+# restore and compile
+
