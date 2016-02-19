4b367043249f4d91ca7a6fb6877be26a644d56ef
Enrico Sada <enrico@sada.io>
2016-02-19 17:35:34 +0100

    travis, install from zip instead of pkg

---
+++ scripts/run-tests.sh
@@ -0,0 +1,56 @@
+#make path absolute
+RootDir = $(dirname $0)
+
+# restore and compile
+
+echo "Testing HelloConsole..."
+
+pushd "${RootDir}/HelloConsole"
+
+dotnet restore
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+dotnet build
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+popd
+
+# dotnet new
+
+echo "Testing dotnet new..."
+
+rm -rf "${RootDir}/test/test-dotnet-new"
+
+mkdir -p "${RootDir}/test/test-dotnet-new" && pushd "${RootDir}/test/test-dotnet-new"
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+dotnet new --lang f#
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+dotnet restore
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+dotnet build
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+dotnet run a b
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+popd
+
+exit 0
